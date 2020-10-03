49df9b33fc2b6b35437ada2d99624b9797539bc4
Miguel Antenor de Oliveira <miguelantoliv@gmail.com>
2020-10-03 19:47:02 -0300

    - Created apiutils package to recover the auth token.

---
+++ apiutils/apiutils.go
@@ -0,0 +1,20 @@
+package apiutils
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/Share-Exp/apiutils/resterrors"
+)
+
+// GetAuthorizationToken receives the request and extracts the token.
+func GetAuthorizationToken(request *http.Request) (*string, resterrors.RestErr) {
+	if request == nil {
+		return nil, resterrors.NewBadRequestError("Request can't be nil.")
+	}
+
+	token := strings.TrimSpace(request.Header["Authorization"][0])
+	token = strings.TrimSpace(strings.ReplaceAll(token, "Bearer", ""))
+
+	return &token, nil
+}
