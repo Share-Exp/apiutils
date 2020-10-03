d913b2759cdf63c7c586ec56fe8a38a07db213e5
Miguel Antenor de Oliveira <miguelantoliv@gmail.com>
2020-10-03 19:58:32 -0300

    - Changed from pointer string to string.

--- apiutils/apiutils.go
+++ apiutils/apiutils.go
@@ -8,13 +8,13 @@ import (
 )
 
 // GetAuthorizationToken receives the request and extracts the token.
-func GetAuthorizationToken(request *http.Request) (*string, resterrors.RestErr) {
+func GetAuthorizationToken(request *http.Request) (string, resterrors.RestErr) {
 	if request == nil {
-		return nil, resterrors.NewBadRequestError("Request can't be nil.")
+		return "", resterrors.NewBadRequestError("Request can't be nil.")
 	}
 
 	token := strings.TrimSpace(request.Header["Authorization"][0])
 	token = strings.TrimSpace(strings.ReplaceAll(token, "Bearer", ""))
 
-	return &token, nil
+	return token, nil
 }
